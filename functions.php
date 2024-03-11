464430ba0f77b9cf33332c1ac77213a946728f43
filippogiovannoni <filippogiovannonii@gmail.com>
2024-03-11 12:35:04 +0100

    add functions for password without repetitions

--- functions.php
+++ functions.php
@@ -12,3 +12,20 @@ function getRandomPassword($length)
 
     return $randomPassword;
 }
+
+function getRandomPasswordWithoutRep($length)
+{
+    $allCharacters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$%/()=?^*-_[]<>';
+    $remainingCharacters = $allCharacters;
+    $randomPassword = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $index = rand(0, strlen($remainingCharacters) - 1);
+        $randCharacter = $remainingCharacters[$index];
+        $randomPassword .= $randCharacter;
+
+        $remainingCharacters = str_replace($randCharacter, '', $remainingCharacters);
+    }
+
+    return $randomPassword;
+}
